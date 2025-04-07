bac6e2d10d7275061d9434119902ab8efc5cec1e
LeonidChetverikov <34073185+LeonidChetverikov@users.noreply.github.com>
2025-04-07 19:45:50 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "azbootcamp" {
   name                     = random_pet.storage_account_name.id
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
@@ -19,6 +19,6 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 
   tags = {
-    environment = "example"
+    environment = "azbootcamp"
   }
 }
